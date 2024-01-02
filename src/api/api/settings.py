4c1fc8ce77412c8d16b956cb5f4a64e839d41ace
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2024-01-02 09:12:02 -0300

    add cors config

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'samples'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
