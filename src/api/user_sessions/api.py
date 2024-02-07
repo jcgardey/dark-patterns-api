5bca1454da4669f36a05c6ab035117ae39526ab4
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2024-02-07 10:14:21 -0300

    assign a website group to a user session when it is created

--- src/api/user_sessions/api.py
+++ src/api/user_sessions/api.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import UserSession
+from websites.models import WebsiteGroup
 from .serializers import UserSessionBriefSerializer, UserSessionFullSerializer
+from django.db.models import Count
 
 class GetUserSessionsAPI(APIView):
     def get(self, request):
@@ -14,7 +16,13 @@ class CreateUserSessionAPI(APIView):
     def post(self, request):
         if (request.data.get('email', None) is None):
             return Response({'error': 'email.invalid' }, status=status.HTTP_400_BAD_REQUEST)
-        user_session = UserSession.objects.create(email=request.data['email'], country=request.data['country'])
+        
+        user_session = UserSession(email=request.data['email'], country=request.data['country'])
+
+        # assign a website group to the user session
+        website_groups = WebsiteGroup.objects.annotate(sessions_count=Count('user_sessions')).order_by('sessions_count', 'order')
+        user_session.website_group = website_groups[0]
+        user_session.save()
         return Response(UserSessionBriefSerializer(user_session).data, status=status.HTTP_201_CREATED)
 
 
--- src/api/user_sessions/models.py
+++ src/api/user_sessions/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from websites.models import WebsiteGroup 
 
 # Create your models here.
 class UserSession(models.Model):
     email = models.EmailField()
     country = models.CharField(max_length=255)
+    website_group = models.ForeignKey(WebsiteGroup, on_delete=models.CASCADE, related_name='user_sessions', null=True)
     
