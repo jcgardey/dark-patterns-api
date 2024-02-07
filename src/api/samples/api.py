dd876c061fc7633d341d669fd24734e18c7256ed
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2024-02-07 17:26:49 -0300

    change website field in Sample to reference Website model

--- src/api/samples/api.py
+++ src/api/samples/api.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .models import Sample
 from .serializers import SampleSerializer
 from user_sessions.models import UserSession
+from websites.models import Website
 
 class GetSamplesAPI(APIView):
     def get(self, request):
@@ -12,9 +13,9 @@ class GetSamplesAPI(APIView):
 
 class CreateSampleAPI(APIView):
    
-   def post(self, request, user_session_id):
+   def post(self, request, user_session_id, website_id):
       sample = UserSession.objects.get(pk=user_session_id).samples.create(
-         website=request.data['website'], 
+         website=Website.objects.get(pk=website_id), 
          dark=request.data['dark'], 
          questionnaire=request.data['questionnaire'], 
          sample_data=request.data['sample_data'] )
--- src/api/samples/models.py
+++ src/api/samples/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from user_sessions.models import UserSession
+from websites.models import Website
 
 # Create your models here.
 class Sample(models.Model):
     user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE, related_name='samples', null=True)
-    website = models.CharField(max_length=255)
+    website = models.ForeignKey(Website, on_delete=models.SET_NULL, null=True)
     dark = models.BooleanField(default=False)
     questionnaire = models.JSONField()
     date = models.DateTimeField(auto_now=True)
