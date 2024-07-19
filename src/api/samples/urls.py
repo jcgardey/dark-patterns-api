5212e6f4d02be53dda9147c3fd47eec02661a7ff
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2024-07-19 14:55:33 -0300

    add export samples endpoint

--- src/api/samples/urls.py
+++ src/api/samples/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from samples.api import GetSamplesAPI, DeleteSampleAPI
+from samples.api import GetSamplesAPI, DeleteSampleAPI, ExportSamplesAPI
 
 urlpatterns = [
     path('', GetSamplesAPI.as_view()),
     path('<int:id>/delete', DeleteSampleAPI.as_view()),
+    path('websites/<int:website_id>/export', ExportSamplesAPI.as_view()),
 ]
