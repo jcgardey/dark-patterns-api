da08ccaa745a06d2d8eb6f158ea0b16d0200cdc1
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2026-05-06 12:21:01 +0000

    fix missing db params

--- src/api/api/settings.py
+++ src/api/api/settings.py
@@ -87,7 +87,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3')
+        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
